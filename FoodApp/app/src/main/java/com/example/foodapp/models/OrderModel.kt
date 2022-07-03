b5ca98ff00fba51acd814b25a7fa75c141dbf52c
Maitry <maitrymakwana196@gmail.com>
2022-07-03 14:06:40 +0530

    03-07-2022

--- FoodApp/app/src/main/java/com/example/foodapp/models/OrderModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodapp.models
-
-class OrderModel (var id:Int,
-                  var name:String,var phone:String,var pic:Int,
-                  var price:String,
-                  var foodname:String,
-                  ) {    }
--- Instagram/app/src/main/java/com/example/instagram/ui/profile/ProfileViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.instagram.ui.profile
-
-import androidx.lifecycle.ViewModel
-
-class ProfileViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-
-}
